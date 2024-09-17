a6fe76c74db4ba7dd5b48f9ed4d705838447b9cc
BrandonAWong <brandonwong2004@outlook.com>
2024-09-17 14:15:49 -0700

    kiten2

--- FileCopy/filecopy.c
+++ FileCopy/filecopy.c
@@ -17,9 +17,11 @@
 #define READ_END 0
 #define WRITE_END 1
 
+// Read and return plaintext of a file descriptor
+// Return NULL if system runs out of memory
 char *getFileContent(int file)
 {
-    size_t size = 10;
+    size_t size = 64;
     char *buf = malloc(size);
     FILE *stream = fdopen(file, "r");
     int i = 0;
@@ -30,10 +32,10 @@ char *getFileContent(int file)
          {
             size *= 2;
             buf = realloc(buf, size);
+            // check if memory is ok
             if (buf == NULL)
             {
-                printf("Ran out of memory");
-                exit(EXIT_FAILURE);
+                return NULL;
             }
          }
          buf[i++] = chr;
@@ -43,68 +45,101 @@ char *getFileContent(int file)
     return buf;
 }
 
-void writeFile(int infile, int outfile)
+// Copy the content of infile into the outfile
+// Return 0 on success, 1 on failure
+int copyFile(int infile, int outfile)
 {
     char *content = getFileContent(infile);
+    if (content == NULL)
+    {
+        return 1;
+    }
     write(outfile, content, strlen(content));
     free(content);
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
+    // verify arguments
     if (argc != 3)
     {
-        printf("wrong amount of arguments ratard.\n");
+        printf("Error: wrong amount of arguments (filecopy SOURCE DESTINATION)\n");
         return EXIT_FAILURE;
     }
     const char *inpath = argv[1];
     const char *outpath = argv[2];
 
+    // verify source and destination are valid
     int infile = open(inpath, O_RDONLY);
     if (infile < 0)
     {
-        printf("Cannot copy '%s': No such file or directory\n", inpath);
+        printf("Error: Cannot copy '%s': No such file or directory\n", inpath);
         return EXIT_FAILURE;
     }
 
     int outfile = open(outpath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (outfile < 0)
     {
-        printf("Cannot create regular file '%s': No such file or directory\n", outpath);
+        close(infile);
+        printf("Error: Cannot create regular file '%s': No such file or directory\n", outpath);
         return EXIT_FAILURE;
     }
 
-    int pipefd[2];  
-    pipe(pipefd); 
+    int pipefd[2];
+    // create and verify pipe
+    if (pipe(pipefd) < 0)
+    {
+        printf("Error: Pipe failed");
+        close(infile);
+        close(outfile);
+        return EXIT_FAILURE;
+    }
 
     pid_t pid = fork();
     if (pid > (pid_t) 0)
     {
-        // parent
+        // parent process
         // reading source and writing to pipe
         close(pipefd[READ_END]);
-        writeFile(infile, pipefd[WRITE_END]);
+        if (copyFile(infile, pipefd[WRITE_END]) > 0)
+        {
+            close(infile);
+            close(outfile);
+            close(pipefd[WRITE_END]);
+            printf("Error: Ran out of memory\n");
+            return EXIT_FAILURE;
+        }
         close(pipefd[WRITE_END]);
         close(infile);
     }
     else if (pid == (pid_t) 0)
     {
-        // child reading pipe and writing to destination
+        // child process
+        // reading pipe and writing to destination
         close(pipefd[WRITE_END]);
-        writeFile(pipefd[READ_END], outfile);
+        if (copyFile(pipefd[READ_END], outfile) > 0)
+        {
+            close(infile);
+            close(outfile);
+            close(pipefd[READ_END]);
+            printf("Error: Ran out of memory\n");
+            return EXIT_FAILURE;
+        }
         close(pipefd[READ_END]);
         close(outfile);
+        printf("File successfully copied from %s to %s\n", inpath, outpath);
     }
     else 
     {
-        printf("Fork failed");
+        printf("Error: Fork failed\n");
         close(infile);
         close(outfile);
         close(pipefd[READ_END]);
         close(pipefd[WRITE_END]);
         return EXIT_FAILURE;
     }
-
+    
     return EXIT_SUCCESS;
 }
 
